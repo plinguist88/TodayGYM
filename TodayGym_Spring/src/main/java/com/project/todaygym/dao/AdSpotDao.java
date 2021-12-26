b517f344cc6f58a1eb1130a58def563e03db55aa
plinguist88 <plinguist88@gmail.com>
2021-12-26 16:35:49 +0900

    Add adminSpot

---
+++ TodayGym_Spring/src/main/java/com/project/todaygym/dao/AdSpotDao.java
@@ -0,0 +1,14 @@
+package com.project.todaygym.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.project.todaygym.dto.SpotDto;
+
+@Mapper
+public interface AdSpotDao {
+	
+	//지점 전체 목록
+	public List<SpotDto> spotAllSelect();
+}
---
+++ TodayGym_Spring/src/main/java/com/project/todaygym/dto/SpotDto.java
@@ -0,0 +1,12 @@
+package com.project.todaygym.dto;
+
+import lombok.Data;
+
+@Data
+public class SpotDto {
+	private String s_code;
+	private String s_name;
+	private String s_phone;
+	private String s_addr;
+	private String s_content;
+}
