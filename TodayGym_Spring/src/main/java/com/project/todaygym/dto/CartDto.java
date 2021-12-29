7fd3a159ad2f2dc3cc06d495fb2b053f4dc48c08
Woody <spacex.ocisly@gmail.com>
2021-12-29 19:19:29 +0900

    Create CartDto.java

---
+++ TodayGym_Spring/src/main/java/com/project/todaygym/dto/CartDto.java
@@ -0,0 +1,13 @@
+package com.project.todaygym.dto;
+
+import lombok.Data;
+
+@Data
+public class CartDto {
+	
+	private int c_code;
+	private String m_id;
+	private String p_code;
+	private String op_code;
+	
+} // class end
