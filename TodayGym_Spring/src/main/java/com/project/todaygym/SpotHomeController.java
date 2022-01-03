27991f0e92b209781075d12036dc27400a44c804
2thyn <seung_hyn@naver.com>
2022-01-03 15:52:10 +0900

    seunghyeon
    backend

--- TodayGym_Spring/src/main/java/com/project/todaygym/SpotHomeController.java
+++ TodayGym_Spring/src/main/java/com/project/todaygym/SpotHomeController.java
@@ -2,21 +2,33 @@ package com.project.todaygym;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.project.todaygym.service.SpotListService;
 
 @Controller
 public class SpotHomeController{
 	
 	private final Logger logger = LoggerFactory.getLogger(SpotHomeController.class);
+	
+	private ModelAndView mv;
+	
+@Autowired SpotListService spotListService;
 
 	//spotHome 페이지로 이동
 	@GetMapping("spotHome")
-	public String spotHome() {
+	public ModelAndView spotHome() {
 		logger.info("spotHome()");
 		
-		return "spot/spotHome";
+		mv = new ModelAndView();
+		
+		mv = spotListService.getSpotList();	
+		return mv; 
 	}
+	//
 	
 	//spotDetail 페이지로 이동
 	@GetMapping("spotDetail")
