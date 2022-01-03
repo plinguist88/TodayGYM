40065842ab50778028f0fde62bd8ec037048ebde
choiminseok911 <95274582+choiminseok911@users.noreply.github.com>
2022-01-03 15:20:08 +0900

    minseok

--- TodayGym_Spring/src/main/java/com/project/todaygym/AdminTicketController.java
+++ TodayGym_Spring/src/main/java/com/project/todaygym/AdminTicketController.java
@@ -30,13 +30,25 @@ public class AdminTicketController {
 		
 		return "admin/adTicketList";
 	}
+	
+	//회원권 관리 이동 및 리스트 출력
+/*	@GetMapping("adTicketMove.ad")
+	public ModelAndView adTicketList(Integer pageNo) {
+		Logger.info("adTicketList()");
+		
+		mv = adTServ.getAdTicketList(pageNo);
+		
+		return mv;
+	}*/
+	
 	//회원권 상세 페이지 이동
 	@GetMapping("adTicketDetailMove.ad")
 	public String adTicketDetailMove() {
-		Logger.info("adTicketDetailMove()");
+	Logger.info("adTicketDetailMove()");
 		
 		return "admin/adTicketDetail";
 	}
+	
 	//회원권 상세 페이지 이동
 //	@GetMapping("adTicketDetailMove.ad")
 //	public ModelAndView adTicketDetailMove2(int t_no) {
--- TodayGym_Spring/src/main/java/com/project/todaygym/service/AdTicketListService.java
+++ TodayGym_Spring/src/main/java/com/project/todaygym/service/AdTicketListService.java
@@ -1,5 +1,11 @@
 package com.project.todaygym.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
@@ -7,6 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.project.todaygym.dao.AdTicketDao;
 import com.project.todaygym.dto.AdTicketDto;
+import com.project.todaygym.util.adPagingUtil;
 
 @Service
 public class AdTicketListService {
@@ -14,12 +21,62 @@ public class AdTicketListService {
 	@Autowired
 	private AdTicketDao adTDao;
 	
+	@Autowired
+	private HttpSession session;
+	
 	private ModelAndView mv;
 	
+	private int lCnt = 10;
+	
+	//임시
 	public Model getTicketList(Model model) {
 		
 		return model;
 	}
+	
+	//회원권 관리 및 리스트 출력
+/*	public ModelAndView getAdTicketList(Integer pageNo) {
+		mv = new ModelAndView();
+		
+		int num = (pageNo == null)? 1 : pageNo;
+		
+		//리스트 가져오기
+		Map<String, Integer> tmap =
+				new HashMap<String, Integer>();
+		tmap.put("num", num);
+		tmap.put("lCnt", lCnt);
+		
+		List<AdTicketDto> tList = adTDao.getAdTList(tmap);
+		
+		mv.addObject("tList", tList);
+		
+		String pageHtml = getPaging(num);
+		mv.addObject("paging", pageHtml);
+		
+		session.setAttribute("pageNo", num);
+		
+		mv.setViewName("admin/adTicketList");
+		
+		return mv;
+	}
+	
+	//페이징 처리
+	private String getPaging(int num) {
+		String pageHtml = null;
+		
+		//전체 글 개수 구하기(DAO)
+		int maxNum = adTDao.getTicketLCnt();
+		//한 페이지에 보여질 페이지 번호 개수
+		int pageCnt = 2;
+		String listName = "list";
+		
+		adPagingUtil paging = new adPagingUtil(maxNum, num, lCnt, 
+				pageCnt, listName);
+		
+		pageHtml = paging.makePaging();
+		
+		return pageHtml;
+	}*/
 	
 	public ModelAndView getTicketDetail(int t_no) {
 		mv = new ModelAndView();
