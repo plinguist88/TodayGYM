9b363795f3429b023e082788f45ab3dc9a69b0a7
choiminseok911 <95274582+choiminseok911@users.noreply.github.com>
2022-01-06 14:58:20 +0900

    minseok

--- TodayGym_Spring/src/main/java/com/project/todaygym/service/AdMainService.java
+++ TodayGym_Spring/src/main/java/com/project/todaygym/service/AdMainService.java
@@ -1,11 +1,14 @@
 package com.project.todaygym.service;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.project.todaygym.dao.AdMainDao;
+import com.project.todaygym.dto.AgeGroupDto;
 
 import lombok.extern.java.Log;
 
@@ -17,12 +20,18 @@ public class AdMainService {
 	
 	private ModelAndView mv;
 	
-	//총원 구하기
+	//Overview 처리
 	public ModelAndView getOverview(ModelAndView mv) {
 		
+		//총원 구하기
 		int cnt = adMainDao.totalCnt();
 		
 		mv.addObject("cnt", cnt);
+		
+		//연령대 구하기
+		List<AgeGroupDto> agDto = adMainDao.ageGroup();
+		
+		mv.addObject("agDto", agDto);
 		
 		return mv;
 	}
