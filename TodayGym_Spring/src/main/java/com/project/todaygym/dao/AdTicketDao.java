ffdee2d72145c2b1b6be35cc2b6e5f2aedeae8b1
choiminseok911 <95274582+choiminseok911@users.noreply.github.com>
2022-01-11 14:03:07 +0900

    minseok

--- TodayGym_Spring/src/main/java/com/project/todaygym/dao/AdTicketDao.java
+++ TodayGym_Spring/src/main/java/com/project/todaygym/dao/AdTicketDao.java
@@ -1,17 +1,9 @@
 package com.project.todaygym.dao;
 
 import java.util.List;
-import java.util.Map;
 
-import com.project.todaygym.dto.AdTicketDto;
+import com.project.todaygym.dto.MyClassDto;
 
 public interface AdTicketDao {
-	
-	//전체 회원권 리스트 가져오기
-	public List<AdTicketDto> getAdTList(Map<String, Integer> tmap);
-	//페이징 처리
-	public int getTicketLCnt();
-	//회원권 상세정보 가져오기
-	public AdTicketDto getTDetail(int t_no);
-
+	public List<MyClassDto> getTicketList();
 }
